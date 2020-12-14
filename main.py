28345aac11b1d8b5747b2f2463a7c00ac4f94579
firstmanonthemoon <40719025+firstmanonthemoon@users.noreply.github.com>
2020-12-14 15:23:46 +0000

    with https and http

--- main.py
+++ main.py
@@ -6,8 +6,19 @@ import time
 import sys
 
 class Scrape():
-    def __init__(self):
+    def __init__(self, main_url):
         self.visited_urls = []
+        self.main_url = main_url
+        if 'https' in self.main_url:
+            self.main_url_https = self.main_url
+        else:
+            self.main_url_https = 'https://' + self.main_url
+
+        if 'http' in self.main_url:
+            self.main_url_http = self.main_url
+        else:
+            self.main_url_http = 'http://' + self.main_url
+            
 
     def tag_visible(self, element):
         if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
@@ -26,21 +37,21 @@ class Scrape():
 
     def get_urls(self, url):
         page = requests.get(url)
-        print(page.status_code)
+        # print(page.status_code)
         soup = BeautifulSoup(page.text, 'html.parser')
         all_anchors = soup.find_all('a',  href=True)
-        usefull_ulrs = [a['href'] for a in all_anchors if a['href'].startswith(url)]
+        usefull_ulrs = [a['href'] for a in all_anchors if a['href'].startswith(self.main_url) or a['href'].startswith(self.main_url_http) or a['href'].startswith(self.main_url_https)]
         return usefull_ulrs
 
-    def get_contents(self, main_url):
+    def get_contents(self, parent_url):
         f = self.f
-        usefull_ulrs = self.get_urls(main_url)
-        if main_url not in self.visited_urls:
-            usefull_ulrs.append(main_url) 
+        usefull_ulrs = self.get_urls(parent_url)
+        if parent_url not in self.visited_urls:
+            usefull_ulrs.append(parent_url) 
 
         usefull_ulrs = list(set(usefull_ulrs))
         if usefull_ulrs:
-            print(f'\ntrying url {main_url} ...')
+            print(f'\ntrying url {parent_url} ...')
 
         for url in usefull_ulrs:
             if url in self.visited_urls:
@@ -64,23 +75,31 @@ class Scrape():
             self.visited_urls.append(url)
             self.get_contents(url)
 
-    def main(self, url):
+    def main(self):
         files_dir = 'files'
         if not os.path.exists(files_dir):
             os.mkdir(files_dir)
-        f_name = url.replace('.', '_').replace(':', '').replace('/', '_') + '.txt'
+        f_name = self.main_url.replace('.', '_').replace(':', '').replace('/', '_') + '.txt'
         
         f_name = os.path.join(files_dir, f_name)
         if os.path.exists(f_name):
             os.remove(f_name)
         # time.sleep(2)
-        if 'http' not in url:
-            url = 'http://' + url
+       
+
         self.f = open(f_name, mode='w+', encoding='utf-8')
         try:
-            self.get_contents(url)
+            print(f'Trying with https')
+            self.get_contents(self.main_url_https)
         except Exception as e:
-            print(e)
+            print(f'cant do it with https: {e}')
+
+            try:
+                print(f' Trying with http')
+                self.get_contents(self.main_url_http)
+            except Exception as e:
+                print(f' cant do it with http: {e}')
+
         # print('Vou fechar!')
         self.f.close()
         print('Data written in file.')
@@ -92,6 +111,6 @@ if __name__ == '__main__':
     # url = "https://loqr.io/"
     # url = "www.nunogsousa.com"
 
-    scrape = Scrape()
-    scrape.main(url)
+    scrape = Scrape(url)
+    scrape.main()
     # scrape.f.close()
